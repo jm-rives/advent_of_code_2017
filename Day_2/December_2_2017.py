df789113f358d37a487379f5ca0a358b25936bf2
Joanna M. Rives <joanna.rives@gmail.com>
2017-12-08 14:09:34 -0800

    ADD working Step 1 (loop through 2d array and select target numbers).

---
+++ Day_2/December_2_2017.py
@@ -0,0 +1,40 @@
+rows = [['5', '1', '9', '5'], ['7', '5', '3'], ['2', '4', '6', '8']]
+big_loop = 0
+little_loop = 0
+differences = []
+
+for row in rows:
+
+    big_num = None
+    little_num = None
+
+    for elem in row:
+        print(f"Current Elem is: {elem}")
+
+        if big_num == None and little_num == None:
+            big_num = int(elem)
+            little_num = int(elem)
+            print(f"Little Num p 'if' is now: {little_num}")
+            print(f"\nBig Num p 'if' is now: {big_num}\n")
+
+        elif int(elem) > big_num:
+            big_num = int(elem)
+            print(f"Little Num p '1st elif' is now: {little_num}\n")
+        elif int(elem) < little_num:
+            little_num = int(elem)
+            print(f"Little Num p '2nd elif' is now: {little_num}\n")
+        else:
+            pass
+
+
+    print(f"\nBig Num is now: {big_num}")
+    print(f"Little Num is now: {little_num}\n")
+    result = int(big_num) - int(little_num)
+    print(f"Result is: {result}\n")
+    differences.append(result)
+
+### TEST ###
+if differences == [8, 4, 6]:
+    print("*PASS* *PASS* *PASS")
+else:
+    print(f"FAIL --> differences = {differences}")
